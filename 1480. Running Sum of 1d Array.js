224179cd9722e43c99311d45daf823dc04cbb5b6
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2021-02-01 16:45:21 +0600

    Modify File

--- 1480. Running Sum of 1d Array.js
+++ 1480. Running Sum of 1d Array.js
@@ -1,19 +1,9 @@
-// Problem Type - Array
+// Problem Type - Array (Easy)
 // 1480. Running Sum of 1d Array
-// Easy
-
-// 942
-
-// 94
-
-// Add to List
-
-// Share
+// Problem Description
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 
 // Return the running sum of nums.
-
- 
 
 // Example 1:
 
